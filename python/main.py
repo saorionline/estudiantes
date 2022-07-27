0e79fac8e05b528a62dc878ab76bed1284aae0ca
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-07-27 12:27:56 -0500

    python version of the objects

---
+++ python/main.py
@@ -0,0 +1,13 @@
+from car import Car
+
+if _name_ == "_main_":
+    print("Rising thinkings List")
+    car = Car ()
+    car.license = "LOL323"
+    car.driver = "Elliot"
+    print(var(car))
+
+    car2 = Car ()
+    car2.license = "LOL323"
+    car2.driver = "Elliot"
+    print(var(car2))
